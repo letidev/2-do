a5747c28760ce580f5f9c7a6a0d8ef184d9f652f
Violeta Malinova <violeta@mlnova.com>
2023-03-20 18:05:55 +0200

    Remove unused import

--- src/utils/guards/UserGuard.tsx
+++ src/utils/guards/UserGuard.tsx
@@ -2,7 +2,6 @@ import React, { FC, PropsWithChildren } from "react";
 import { Navigate } from "react-router-dom";
 import { PATHS } from "../constants";
 import { getLoggedUserId } from "../http-utils/user-requests";
-import { User } from "../types/user";
 
 const UserGuard: FC<PropsWithChildren> = ({ children }) => {
   const userId = getLoggedUserId();
