6a64055ce2e0949b8179d6e0f984e9e1c531bb83
Violeta Malinova <violeta@mlnova.com>
2023-03-22 16:54:06 +0200

    Fix typo

--- src/components/common/inputs/OutlineButton.tsx
+++ src/components/common/inputs/OutlineButton.tsx
@@ -9,7 +9,7 @@ const OutlineButton: FC<Props> = ({ text, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="flex justify-center w-full px-4 py-2 text-sm font-medium text-indigo-700 bg-transparent border border-transparent border-indigo-500 rounded-md shadow-sm hover:bg-indigo-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      className="flex justify-center w-full px-4 py-2 text-sm font-medium text-indigo-700 bg-transparent border border-indigo-500 rounded-md shadow-sm hover:bg-indigo-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
     >
       {text}
     </button>
